3b40c32beec7a75864b91983e00537d51509339a
PaulKon <paulkon68@gmail.com>
2025-03-02 20:11:47 +0200

    Update PingSweepDetector.py

--- Ping_Sweep/PingSweepDetector.py
+++ Ping_Sweep/PingSweepDetector.py
@@ -8,10 +8,13 @@ class PingSweepDetector:
     def __init__(self, gui: None = None, ip_stats: dict[str, int] = {}):
         self.IP_ICMP_Stats = ip_stats  # Store the ICMP stats, if provided
         self.gui = gui  # store the GUI class instance, if provided
+        self.start_time = 0
+        self.help_check_time = 0
 
     def process_packets(self, packet):
 
-        if packet.haslayer("ICMP"):
+        # Filter for ICMP request packets
+        if packet.haslayer("ICMP") and packet.getlayer("ICMP").type == 8:
 
             # ICMP packet source IP
             icmp_src_ip = packet.getlayer('IP').src
@@ -19,10 +22,27 @@ class PingSweepDetector:
             # Set the default value to zero, if it does not exist
             self.IP_ICMP_Stats.setdefault(icmp_src_ip, 0)
 
+            # ---
+
+            # Check if the ping request time frame is bigger than 30 seconds, reset the counter
+
+            if self.help_check_time == 0:
+                self.start_time = datetime.datetime.now()
+                self.help_check_time = 1
+
+            y = datetime.datetime.now()
+            print(f"y: {y}, start_time: {self.start_time}, {self.IP_ICMP_Stats[icmp_src_ip]}")
+
+            if (y - self.start_time).seconds > 30:
+                self.IP_ICMP_Stats[icmp_src_ip] = 0
+                self.start_time = y
+
+            # ---
+
             # Increase the value of ICMP packets sent from a source IP
             self.IP_ICMP_Stats[icmp_src_ip] += 1
 
-            # If ICMP messages pass a specific threshold
+            # If ICMP messages pass a specific limit
             if int(self.IP_ICMP_Stats[icmp_src_ip]) >= 3:
 
                 # Record the time of event
